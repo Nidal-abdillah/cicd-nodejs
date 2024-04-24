ef84c8f0d1f861fa888677590d6529b1e4bde679
Nidal abdillah <Nidalallabdillah@gmail.com>
2024-04-24 15:13:03 +0700

    tes sukses

--- app.test.js
+++ app.test.js
@@ -3,5 +3,5 @@ const sayHello = require("./app");
 
 test("kelmbalikan salam yang benar", () => {
   const result = sayHello();
-  expect(result).toBe("hello, world!");
+  expect(result).toBe("Hello, world!");
 });
